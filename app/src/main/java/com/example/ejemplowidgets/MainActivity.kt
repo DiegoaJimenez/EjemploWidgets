92e724b5fcc42a1c4dff895a35d971666e038ee1
goslip05 <djimenez750@uan.edu.co>
2023-04-15 22:34:37 -0500

    evente listener with click button change image

--- app/src/main/java/com/example/ejemplowidgets/MainActivity.kt
+++ app/src/main/java/com/example/ejemplowidgets/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.ejemplowidgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.ejemplowidgets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imgTeam.setImageResource(R.drawable.realmadrid)
+
+        binding.btnLiverpool.setOnClickListener { changeTeam("liverpool") }
+        binding.btnRealmadrid.setOnClickListener { changeTeam("realmadrid") }
+    }
+
+    fun changeTeam(teamName:String){
+        //Toast.makeText(this,"You choose ${teamName.capitalize()}",Toast.LENGTH_SHORT).show()
+        val id = resources.getIdentifier(teamName,"drawable",packageName)
+        binding.imgTeam.setImageResource(id)
+
     }
 }
