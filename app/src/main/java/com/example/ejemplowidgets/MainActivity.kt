366f0f453f3023d6b6db691133d46ae4bc3f86d9
goslip05 <djimenez750@uan.edu.co>
2023-04-15 20:40:01 -0500

    Add Binding

--- app/src/main/java/com/example/ejemplowidgets/MainActivity.kt
+++ app/src/main/java/com/example/ejemplowidgets/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.ejemplowidgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejemplowidgets.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
